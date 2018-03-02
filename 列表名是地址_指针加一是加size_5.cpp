45a335e1d27e8d627ce1bc4719d1d30582e25689
tyxr <751021032@qq.com>
2018-03-02 15:21:02 +0800

    Add files via upload

---
+++ 列表名是地址_指针加一是加size_5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void print( int *pBegin, int *pEnd )
+{
+	while( pBegin != pEnd )
+	{
+		std::cout << *pBegin;
+		++pBegin;
+	}
+}
+
+void print( char *pBegin, char *pEnd )
+{
+	while( pBegin != pEnd )
+	{
+		std::cout << *pBegin;
+		++pBegin;
+	}
+}
+
+int main()
+{
+	int num[5] = { 0, 1, 2, 3, 4 };
+	char name[5] = { 'H', 'E', 'L', 'L', 'O' };
+	
+	print( num, num+5 );
+	std::cout << '\n';
+	print( name, name+5 );
+	std::cout << '\n';
+	
+	return 0;
+}
+
